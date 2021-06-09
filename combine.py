8021c48c4d2ca1ae0dbfef6c442738c07258bf76
Priyak5 <32927328+Priyak5@users.noreply.github.com>
2021-06-09 13:03:08 +0530

    Add files via upload

--- combine.py
+++ combine.py
@@ -78,94 +78,92 @@ class smith_waterman(object):
 
 
 import textdistance
-# sw = smith_waterman()
-class String_matching:
-    def lcs(X,Y): 
-        m = len(X) 
-        n = len(Y) 
+def lcs(X,Y): 
+    m = len(X) 
+    n = len(Y) 
 
-        L = [[None]*(n+1) for i in range(m+1)] 
+    L = [[None]*(n+1) for i in range(m+1)] 
 
-        for i in range(m+1): 
-            for j in range(n+1): 
-                if i == 0 or j == 0 : 
-                    L[i][j] = 0
-                elif X[i-1] == Y[j-1]: 
-                    L[i][j] = L[i-1][j-1]+1
-                else: 
-                    L[i][j] = max(L[i-1][j] , L[i][j-1]) 
-        return L[m][n]/(max(len(X),len(Y)))
-    
-    def iterative_levenshtein(s, t):
-        rows = len(s)+1
-        cols = len(t)+1
-        dist = [[0 for x in range(cols)] for x in range(rows)]
+    for i in range(m+1): 
+        for j in range(n+1): 
+            if i == 0 or j == 0 : 
+                L[i][j] = 0
+            elif X[i-1] == Y[j-1]: 
+                L[i][j] = L[i-1][j-1]+1
+            else: 
+                L[i][j] = max(L[i-1][j] , L[i][j-1]) 
+    return L[m][n]/(max(len(X),len(Y)))
 
-        for i in range(1, rows):
-            dist[i][0] = i
+def iterative_levenshtein(s, t):
+    rows = len(s)+1
+    cols = len(t)+1
+    dist = [[0 for x in range(cols)] for x in range(rows)]
 
-        for i in range(1, cols):
-            dist[0][i] = i
-            
-        for col in range(1, cols):
-            for row in range(1, rows):
-                if s[row-1] == t[col-1]:
-                    cost = 0
-                else:
-                    cost = 1
-                dist[row][col] = min(dist[row-1][col] + 1,dist[row][col-1] + 1,dist[row-1][col-1] + cost) 
+    for i in range(1, rows):
+        dist[i][0] = i
+
+    for i in range(1, cols):
+        dist[0][i] = i
         
-        return dist[row][col]/(max(len(s),len(t)))
+    for col in range(1, cols):
+        for row in range(1, rows):
+            if s[row-1] == t[col-1]:
+                cost = 0
+            else:
+                cost = 1
+            dist[row][col] = min(dist[row-1][col] + 1,dist[row][col-1] + 1,dist[row-1][col-1] + cost) 
+    
+    return dist[row][col]/(max(len(s),len(t)))
 
-    def hamming_distance(string1, string2):
-        try:
-            distance = 0
-            L = len(string1)
-            for i in range(L):
-                if string1[i] != string2[i]:
-                    distance += 1
-            return distance/(max(len(string1),len(string2)))
-        except:
-            return textdistance.hamming.normalized_similarity(string1,string2)
-    
-    def smith_waterman(string1,string2):
-        return textdistance.smith_waterman.normalized_similarity(string1,string2)
-    
-    def jaro_winkler(string1,string2):
-        return textdistance.jaro_winkler.normalized_similarity(string1,string2)
-    
-    def Strcmp95(string1,string2):
-        return textdistance.strcmp95.normalized_similarity(string1,string2)
-    
-    def Needleman_wunsch(string1,string2):
-        return textdistance.needleman_wunsch.normalized_similarity(string1,string2)
+def hamming_distance(string1, string2):
+    try:
+        distance = 0
+        L = len(string1)
+        for i in range(L):
+            if string1[i] != string2[i]:
+                distance += 1
+        return distance/(max(len(string1),len(string2)))
+    except:
+        return textdistance.hamming.normalized_similarity(string1,string2)
 
-    def gotoh(string1,string2):
-        return textdistance.gotoh.normalized_similarity(string1,string2)
-    
-    def jaccard(string1,string2):
-        return textdistance.jaccard.normalized_similarity(string1,string2)
-    
-    def sorensen_dice(string1,string2):
-        return textdistance.sorensen_dice.normalized_similarity(string1,string2)
-    
-    def tversky(string1,string2):
-        return textdistance.tversky.normalized_similarity(string1,string2)
-    
-    def overlap(string1,string2):
-        return textdistance.overlap.normalized_similarity(string1,string2)
-    
-    def tanimoto(string1,string2):
-        return textdistance.tanimoto.normalized_similarity(string1,string2)
-    
-    def cosine(string1,string2):
-            return textdistance.cosine.normalized_similarity(string1,string2)
-    
-    def mra(string1,string2):
-            return textdistance.mra.normalized_similarity(string1,string2)
-    
-    def editex(string1,string2):
-            return textdistance.editex.normalized_similarity(string1,string2)
+def smith_waterman(string1,string2):
+    return textdistance.smith_waterman.normalized_similarity(string1,string2)
+
+def jaro_winkler(string1,string2):
+    return textdistance.jaro_winkler.normalized_similarity(string1,string2)
+
+def Strcmp95(string1,string2):
+    return textdistance.strcmp95.normalized_similarity(string1,string2)
+
+def Needleman_wunsch(string1,string2):
+    return textdistance.needleman_wunsch.normalized_similarity(string1,string2)
+
+def gotoh(string1,string2):
+    return textdistance.gotoh.normalized_similarity(string1,string2)
+
+def jaccard(string1,string2):
+    return textdistance.jaccard.normalized_similarity(string1,string2)
+
+def sorensen_dice(string1,string2):
+    return textdistance.sorensen_dice.normalized_similarity(string1,string2)
+
+def tversky(string1,string2):
+    return textdistance.tversky.normalized_similarity(string1,string2)
+
+def overlap(string1,string2):
+    return textdistance.overlap.normalized_similarity(string1,string2)
+
+def tanimoto(string1,string2):
+    return textdistance.tanimoto.normalized_similarity(string1,string2)
+
+def cosine(string1,string2):
+        return textdistance.cosine.normalized_similarity(string1,string2)
+
+def mra(string1,string2):
+        return textdistance.mra.normalized_similarity(string1,string2)
+
+def editex(string1,string2):
+        return textdistance.editex.normalized_similarity(string1,string2)
 import pandas as pd
 import pyreadstat
 import seaborn as sn
@@ -413,14 +411,11 @@ if analysis=='Data Quality':
 	from nltk.stem import WordNetLemmatizer
 	from nltk.corpus import stopwords
 	from nltk.tokenize import word_tokenize
-	nltk.download('stopwords')
-	nltk.download('wordnet')
-	nltk.download('punkt')
 	sm = String_matching()
 	p = inflect.engine()
 	data= metadata['codebook_desc']
-	similarity_metrics=[sm.hamming_distance,sm.cosine,sm.smith_waterman,sm.jaccard,sm.jaro_winkler,sm.Needleman_wunsch,sm.Strcmp95,sm.gotoh,sm.sorensen_dice,sm.tversky,sm.overlap,sm.tanimoto,sm.mra,sm.editex]
-	print('Trying 13 string matching metrics including edit distance, token based, sequence based and phonetic based')
+	similarity_metrics=[lcs,hamming_distance,cosine,smith_waterman,jaccard,jaro_winkler,Needleman_wunsch,Strcmp95,gotoh,sorensen_dice,tversky,overlap,tanimoto,mra,editex]
+	print('Trying 17 string matching metrics including edit distance, token based, sequence based and phonetic based')
 	labels= metadata['dataset_desc']
 	similarity_order={}
 	def Sort_Tuple(tup):  
@@ -479,6 +474,10 @@ if analysis=='Data Quality':
 		z = x.intersection(y)
 		temp1 = " ".join(z)
 		temp2 = " ".join(x)
+		# print("start lol")
+		# print(temp1)
+		# print(temp2)
+		# print("end lol")
 		metric_score=0
 		for k in similarity_metrics:
 			if k!=sm.iterative_levenshtein and k!=sm.hamming_distance and k!=sm.tanimoto:
